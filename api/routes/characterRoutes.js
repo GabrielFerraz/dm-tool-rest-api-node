a5880f9532b3c0787f4d010d04fb207e1cae265b
GabrielFerraz <gabrielmaiaferraz@gmail.com>
2018-02-07 19:16:40 +1300

    basic character routes

---
+++ api/routes/characterRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var character = require('../controllers/characterController');
+
+  // todoList Routes
+  app.route('/characters')
+    .get(character.list_all_chars)
+    .post(character.create_a_char);
+
+
+  app.route('/characters/:characterId')
+    .get(character.read_a_char)
+    .put(character.update_a_char)
+    .delete(character.delete_a_char);
+};
