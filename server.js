2583a66ffd0ab4e433707778e012e60efb3c4f65
GabrielFerraz <gabrielmaiaferraz@gmail.com>
2018-02-07 19:25:45 +1300

    server running basic character routes

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+  mongoose = require('mongoose'),
+  Task = require('./api/models/characterModel'), //created model loading here
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Dmtooldb'); 
+
+app.use(bodyParser.urldecoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/characterRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
