65ad50990ec60f3822c9a3b65a0c65f7b49857d4
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2018-03-07 22:31:19 +1300

    added CORS to the server

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ var express = require('express'),
   Weapon = require('./api/models/weaponModel'), //created model loading here
   Weapon = require('./api/models/armorModel'), //created model loading here
   bodyParser = require('body-parser');
+const cors = require('cors');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Dmtooldb'); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
 
 var routes = require('./api/routes/dmToolRoutes'); //importing route
 routes(app); //register the route
